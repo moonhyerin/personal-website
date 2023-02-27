48154b28be26470c1a68953208d0d174cbf6a7e0
unknown <hrmoon081632@gmail.com>
2023-02-27 17:39:25 +0900

    feat: divide index paged

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,41 @@
 import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import { useRef } from "react";
 import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
+import Header from "../components/header";
+import styles from "../styles/utils.module.css";
+
+const sectionStyle = {
+  height: "100vh",
+  width: "100%",
+  fontSize: "40px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
 
 export default function Home() {
+  const homeRef = useRef();
+  const aboutMeRef = useRef();
+  const workingRef = useRef();
+  const contactRef = useRef();
+
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+    <div className={styles.container}>
+      <Header style={styles.header} />
+      <section ref={homeRef} style={sectionStyle} id="home">
+        home
       </section>
-    </Layout>
+      <section ref={aboutMeRef} style={sectionStyle} id="aboutme">
+        about me
+      </section>
+      <section ref={workingRef} style={sectionStyle} id="working">
+        working
+      </section>
+      <section ref={contactRef} style={sectionStyle} id="contact">
+        contact
+      </section>
+    </div>
   );
 }
