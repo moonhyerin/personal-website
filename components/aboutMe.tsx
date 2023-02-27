e9bbdb75b34762182754d3c3d04053b4542a343d
unknown <hrmoon081632@gmail.com>
2023-02-27 23:11:13 +0900

    feat: add aboutMe component

---
+++ components/aboutMe.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Image from "next/image";
+
+import SkillCard from "./SkillCard";
+
+import aboutMeStyles from "./aboutMe.module.css";
+
+const skillSets = [
+  "Javascript",
+  "React",
+  "Node.js",
+  "HTML/CSS",
+  "Typescript",
+  "Git",
+  "TDD",
+  "Figma",
+];
+
+export default function AboutMe() {
+  return (
+    <div className={aboutMeStyles.aboutMe}>
+      <Image
+        className={aboutMeStyles.profile}
+        src="/images/profile.jpg"
+        width={310}
+        height={470}
+        alt=""
+      />
+      <div className={aboutMeStyles.skills}>
+        {skillSets.map((skill) => (
+          <SkillCard skillName={skill} />
+        ))}
+      </div>
+    </div>
+  );
+}
