a3482e10e38d870bf6d3d4b2001f942a3e8c1999
unknown <hrmoon081632@gmail.com>
2023-03-14 11:59:20 +0900

    fix: remove unnecessary function and import

--- components/WorkingPage.tsx
+++ components/WorkingPage.tsx
@@ -1,35 +1,45 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import { ThemeProvider } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
 
 import styles from './WorkingPage.module.css';
 
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
-}
+const CustomFontTheme = createTheme({
+  typography: {
+    fontSize: 15,
+    fontFamily: ['Roboto Mono, monospace'].join(','),
+  },
+});
 
 export default function WorkingPage() {
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
   const renderCard = () => {
     return [1, 2, 3].map((i) => (
-      <Card className={styles.card} sx={{ maxWidth: 345, height: 400 }}>
-        <CardContent>
-          <Typography variant='body2' color='text.secondary'>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita
-            sed suscipit, quasi voluptas itaque hic ipsum corrupti nulla aut non
-            mollitia ex, harum obcaecati voluptatem numquam sint aspernatur,
-            reiciendis perferendis!
-          </Typography>
-        </CardContent>
-      </Card>
+      <ThemeProvider theme={CustomFontTheme}>
+        <Card
+          className={styles.card}
+          sx={{
+            ':hover': { boxShadow: 10 },
+            maxWidth: 345,
+            height: 400,
+          }}
+        >
+          <CardContent>
+            <Typography
+              variant='body2'
+              color='text.secondary'
+              sx={{ ':hover': { fontWeight: 'bold' } }}
+            >
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita
+              sed suscipit, quasi voluptas itaque hic ipsum corrupti nulla aut
+              non mollitia ex, harum obcaecati voluptatem numquam sint
+              aspernatur, reiciendis perferendis!
+            </Typography>
+          </CardContent>
+        </Card>
+      </ThemeProvider>
     ));
   };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // `pages/_app.js`
 import React, { useEffect } from 'react';
+
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
